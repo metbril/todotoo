4224a2c6d8e10c6eb90098d69b35f6cbd6f35502
Robert van Bregt <robert@robertvanbregt.nl>
2015-05-22 15:31:14 +0200

    Read file locations from config

--- todo.py
+++ todo.py
@@ -18,6 +18,7 @@
 
 import sys
 import argparse
+import ConfigParser
 
 __version__   = "0.0.1"
 __date__      = "2015-05-22"
@@ -27,8 +28,51 @@ __license__   = "GPL"
 
 def todo_arguments():
     parser = argparse.ArgumentParser(description='Manage your TODO.TXT')
-    args = parser.parse_args()
-    
+    parser.add_argument('-d', dest='config_file', default='./todo.conf',
+                        help='path to todo.txt')
+    parser.add_argument('action', 
+                        help='the action to execute')
+    return parser.parse_args()
+ 
+def todo_config(config_file):
+    config = ConfigParser.ConfigParser()
+    config.read(config_file)
+    return config
+
+def read_todos(todo_filepath):
+    todo_file = open(todo_filepath, 'r')
+    raw_todos = todo_file.readlines()
+    todo_file.close()
+    todos = []
+    for item in raw_todos:
+        item = item.strip("\n")
+        todos.append(item)
+    return todos
+
+def list_todos(results, numbers = 1):
+	if len(results) == 0:
+	    #empty list
+		print("No results.")
+	else:
+		i = 1
+		for item in results:
+			if numbers == 1:
+				print("{0}. {1}".format(i, item))
+			else:
+				print(item)
+			i += 1
+
 if __name__ == "__main__":
     args = todo_arguments()
+    
+    config = todo_config(args.config_file)
+
+    todo_path = config.get('files', 'todo_dir')
+    # TODO: only add slash if required
+    todo_filepath = todo_path + "/" + config.get('files', 'todo_file')
+
+    # at this time, just list file contents
+    todos = read_todos(todo_filepath)
+    list_todos(todos)
+    
     sys.exit()
